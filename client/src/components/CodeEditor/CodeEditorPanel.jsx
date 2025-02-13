e500fa133bd956907e0322249eaadc75ced70f20
Aditya Sutar <aditya1.sutar@outlook.com>
2025-02-13 22:45:04 +0530

    changed panel size

--- client/src/components/CodeEditor/CodeEditorPanel.jsx
+++ client/src/components/CodeEditor/CodeEditorPanel.jsx
@@ -39,7 +39,7 @@ function CodeEditorPanel() {
     <Box height="100vh" width="100%">
       <PanelGroup direction="vertical">
         {/* code editor */}
-        <Panel defaultSize={60} maxSize={100}>
+        <Panel defaultSize={65} maxSize={100}>
           <Box height="100%">
             <LanguageSelect
               languages={languages}
