ede45550998237a2fc309e168deac0500285d2af
avanee <avanikhot16@gmail.com>
2025-03-02 17:54:10 +0530

    created basic dropdown for themes
    yoooooo

--- client/src/components/CodeEditor/ThemeSelect.jsx
+++ client/src/components/CodeEditor/ThemeSelect.jsx
@@ -1,3 +1,20 @@
-function ThemeSelect() {}
+import { DropdownMenu, Button } from "@radix-ui/themes";
+
+function ThemeSelect() {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Button variant="soft">
+          Options
+          <DropdownMenu.TriggerIcon />
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Item>Nord</DropdownMenu.Item>
+        <DropdownMenu.Item>Github Dark </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
 
 export default ThemeSelect;
