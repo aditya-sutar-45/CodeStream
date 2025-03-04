4218ecf0e4d56a65ea888f04b5ba9947fbb81993
avanee <avanikhot16@gmail.com>
2025-03-04 15:22:28 +0530

    Addition to themesSelect Dropdown

--- client/src/components/CodeEditor/ThemeSelect.jsx
+++ client/src/components/CodeEditor/ThemeSelect.jsx
@@ -19,8 +19,22 @@ function ThemeSelect({theme, themes, onSelect}) {
           </Button>
         </DropdownMenu.Trigger>
         <DropdownMenu.Content>
-          <DropdownMenu.Item>Nord</DropdownMenu.Item>
-          <DropdownMenu.Item>Github Dark </DropdownMenu.Item>
+          {themes.length > 0 ? (
+            themes.map ((t,i) => (
+              <DropdownMenu.Item
+                key={i}
+                onSelect={() => onSelect(t)}
+                data-state={theme === t ? "selected" : ""}
+                style={{
+                  color: theme === t ? "grey" : "inherit",
+                }}
+                >
+                  {t}
+                </DropdownMenu.Item>
+            ))) : (
+              <DropdownMenu.Item disabled>No Themes</DropdownMenu.Item>
+            )}
+              
         </DropdownMenu.Content>
       </DropdownMenu.Root>
     </Box>
