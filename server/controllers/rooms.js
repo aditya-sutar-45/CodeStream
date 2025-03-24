e464b05b75fd57af8f03e84412c32921700e84b0
Aditya Sutar <aditya1.sutar@outlook.com>
2025-03-24 23:41:00 +0530

    cleaned up handleSocket.js

---
+++ server/controllers/rooms.js
@@ -0,0 +1,54 @@
+import { v4 as uuid } from "uuid";
+
+export const handleCreateRoom = (
+  socket,
+  rooms,
+  { username, roomName, roomPassword }
+) => {
+  console.log(
+    `${username}, wants to create a room with\nroom name: ${roomName}\npassword: ${roomPassword}`
+  );
+
+  const roomId = uuid();
+
+  const newRoom = {
+    roomId,
+    roomPassword,
+    roomName,
+    owner: { socketId: socket.id, username },
+    users: [{ socketId: socket.id, username }],
+  };
+
+  rooms.push(newRoom);
+  console.log(rooms);
+  socket.join(roomId);
+
+  console.log(`room created by ${username} with id: ${roomId}`);
+  socket.emit("roomCreated", roomId);
+};
+
+export const getRoomInfo = (socket, roomId, rooms) => {
+  const room = rooms.find((room) => room.roomId === roomId);
+
+  if (!room) {
+    socket.emit("roomError", "room not found!");
+    return;
+  }
+
+  socket.emit("roomInfo", room);
+};
+
+export const handleDisconnect = (socket, rooms, io) => {
+  console.log(`user disconnected: ${socket.id}`);
+
+  rooms.forEach((room, index) => {
+    room.users = room.users.filter((user) => user.socketId !== socket.id);
+    if (room.users.length === 0) {
+      rooms.splice(index, 1);
+      console.log(`room ${room.roomId} deleted`);
+    }
+    console.log(rooms);
+  });
+
+  io.emit("roomList", rooms);
+};
--- server/handleSocket.js
+++ server/handleSocket.js
@@ -1,4 +1,8 @@
-import { v4 as uuid } from "uuid";
+import {
+  getRoomInfo,
+  handleCreateRoom,
+  handleDisconnect,
+} from "./controllers/rooms.js";
 
 const rooms = [];
 
@@ -6,53 +10,16 @@ const handleSocket = (io) => {
   io.on("connection", (socket) => {
     console.log(`user connected: ${socket.id}`);
 
-    socket.on("createRoom", ({ username, roomName, roomPassword }) => {
-      console.log(
-        `${username}, wants to create a room with\nroom name: ${roomName}\npassword: ${roomPassword}`
-      );
-
-      const roomId = uuid();
-
-      const newRoom = {
-        roomId,
-        roomPassword,
-        roomName,
-        owner: { socketId: socket.id, username },
-        users: [{ socketId: socket.id, username }],
-      };
-
-      rooms.push(newRoom);
-      console.log(rooms);
-      socket.join(roomId);
-
-      console.log(`room created by ${username} with id: ${roomId}`);
-      socket.emit("roomCreated", roomId);
+    socket.on("createRoom", (data) => {
+      handleCreateRoom(socket, rooms, data);
     });
 
-    socket.on("getRoomInfo", ({roomId}) => {
-      const room = rooms.find((room) => room.roomId === roomId);
-
-      if (!room) {
-        socket.emit("roomError", "room not found!");
-        return;
-      }
-
-      socket.emit("roomInfo", room);
-    })
+    socket.on("getRoomInfo", ({ roomId }) => {
+      getRoomInfo(socket, roomId, rooms);
+    });
 
     socket.on("disconnect", () => {
-      console.log(`user disconnected: ${socket.id}`);
-
-      rooms.forEach((room, index) => {
-        room.users = room.users.filter((user) => user.socketId !== socket.id);
-        if (room.users.length === 0) {
-          rooms.splice(index, 1);
-          console.log(`room ${room.roomId} deleted`);
-        }
-        console.log(rooms);
-      });
-
-      io.emit("roomList", rooms);
+      handleDisconnect(socket, rooms, io);
     });
   });
 };
