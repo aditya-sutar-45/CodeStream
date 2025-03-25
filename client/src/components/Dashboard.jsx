4625d3c382a8c43a76074cf6f5c0e4c25b6c3a9c
avanee <avanikhot16@gmail.com>
2025-03-25 15:52:03 +0530

    Whiteboard Changes
    added animation to icons

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,22 +2,18 @@ import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import {
-  // Heading,
   Box,
-  // Card,
   Flex,
   Avatar,
-  Text,
+  Heading,
   Button,
   Callout,
+  Container,
 } from "@radix-ui/themes";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import NavHeader from "./Home/NavHeader";
 
 export default function UserProfile() {
-  // const [name, setName] = useState("John Doe");
-  // const [bio, setBio] = useState("Software Developer at XYZ");
-  // const [isEditing, setIsEditing] = useState(false);
   const [error, setError] = useState("");
   const { currentUser, username, logout } = useAuth();
   const navigate = useNavigate();
@@ -33,83 +29,50 @@ export default function UserProfile() {
   }
 
   return (
-    <Box style={{ height: "100vh", overflow: "hidden" }}>
+    <Box style={{ overflow: "hidden" }}>
       <NavHeader />
-      <Flex justify="center" align="center" height="80vh">
-        {error && (
-          <Callout.Root color="red" style={{ marginTop: "16px" }}>
-            <Callout.Icon>
-              <InfoCircledIcon />
-            </Callout.Icon>
-            <Callout.Text>{error}</Callout.Text>
-          </Callout.Root>
-        )}
+      <Container size="2" height="40vh">
+        <Flex mt="3rem" height="40vh" justify="center" align="center" gap="5">
+          {error && (
+            <Callout.Root color="red" style={{ marginTop: "1rem" }}>
+              <Callout.Icon>
+                <InfoCircledIcon />
+              </Callout.Icon>
+              <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>
+          )}
 
-        <Box mt="4">
-          <Avatar
-            size="9"
-            radius="full"
-            fallback={username[0].toUpperCase()}
-            style={{
-              width: "250px",
-              height: "250px",
-              marginLeft: "-90%",
-              marginTop: "-90%",
-            }}
-          />
-        </Box>
-        <Box mt="400px">
-          <Flex
-            gap="4"
-            justify="center"
-            style={{ marginLeft: "58.5%", marginBottom: "190%" }}
-          >
-            <Button
-              color="crimson"
-              style={{ width: "250%", marginLeft: "-250.5%" }}
-              my="1"
-            >
-              Edit Profile
-            </Button>
-            <Button
-              onClick={handleLogout}
-              style={{ width: "250%", marginLeft: "2.5%" }}
-              my="1"
-            >
-              Logout
-            </Button>
+          <Box style={{ width: "50%", textAlign: "center" }}>
+            <Avatar size="9" radius="full" fallback={username[0].toUpperCase()}
+              style={{ justifyContent: "center", width: "15rem", height: "15rem" }} />
+          </Box>
+
+          <Flex width="30vw">
+            <Box>
+              <Heading weight="bold" as="h1" size="9">{username}</Heading>
+              <Heading weight="regular" as="h2" size="5" style={{ color: "gray"}}>{currentUser.email}
+              </Heading>
+              <Flex justify="start" gap="2" mt="6rem" >
+                <Button color="crimson" style={{ width:"8rem" }}>
+                  Edit Profile
+                </Button>
+
+                <Button onClick={handleLogout} style={{ width: "8rem" }}>
+                  Logout
+                </Button>
+              </Flex>
+            </Box>
           </Flex>
-        </Box>
-
-        <Box
-          textAlign="centre"
-          style={{
-            marginBottom: "20%",
-            display: "flex",
-            flexDirection: "column",
-            gap: "5px",
-            textAlign: "left",
-            marginLeft: "-46.5%",
-            paddingLeft: "20%",
-          }}
-        >
-          <Text as="div" size="9" weight="bold">
-            {username}
-          </Text>
-          <Text as="div" size="5" color="gray">
-            {currentUser.email}
-          </Text>
-        </Box>
-      </Flex>
-
+        </Flex>
+      </Container>
       <div
         style={{
-          height: "2px",
-          width: "65%",
+          height: "0.1rem",
+          width: "55%",
           backgroundColor: "gray",
-          margin: "-16% 18%",
+          margin: "3% auto",
         }}
       />
     </Box>
   );
-}
+}    
