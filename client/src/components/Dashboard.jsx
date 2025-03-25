012dea6ba4ba421071ed0849cc37e7eabbf6d5f9
Aditya Sutar <aditya1.sutar@outlook.com>
2025-03-25 15:49:07 +0530

    Update Dashboard.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -32,43 +32,69 @@ export default function UserProfile() {
     <Box style={{ overflow: "hidden" }}>
       <NavHeader />
       <Flex height="100%" width="100%" justify="center">
-      <Box height="100vh" width="60vw">
-        <Flex mt="3rem" height="50vh" justify="between" align="center">
-          {error && (
-            <Callout.Root color="red" style={{ marginTop: "1rem" }}>
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>
-          )}
+        <Box height="100vh" width="95vw">
+          <Flex mt="3rem" height="50vh" justify="between" align="center">
+            {error && (
+              <Callout.Root color="red" style={{ marginTop: "1rem" }}>
+                <Callout.Icon>
+                  <InfoCircledIcon />
+                </Callout.Icon>
+                <Callout.Text>{error}</Callout.Text>
+              </Callout.Root>
+            )}
 
-          <Box width="40%" style={{textAlign: "end"}} mx="3">
-            <Avatar size="9" src="https://images.unsplash.com/photo-1481214110143-ed630356e1bb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" radius="full" fallback={username[0].toUpperCase()}
-              style={{  width: "20rem", height: "20rem" }} />
-          </Box>
-
-          {/* <Separator orientation="vertical" style={{height: "90%"}} /> */}
-
-          <Flex width="40%" height="100%" justify="start" mx="3" align="center">
-            <Box>
-              <Heading weight="bold" as="h1" style={{fontSize: "3rem", height: "40%"}} my="4">{username}</Heading>
-              <Heading weight="regular" as="h2" size="4" style={{ color: "gray"}} my="4">{currentUser.email}
-              </Heading>
-              <Flex justify="start" gap="2" mt="6rem" >
-                <Button color="crimson" style={{ width:"8rem" }}>
-                  Edit Profile
-                </Button>
-
-                <Button onClick={handleLogout} style={{ width: "8rem" }}>
-                  Logout
-                </Button>
-              </Flex>
+            <Box width="40%" style={{ textAlign: "end" }} mx="3">
+              <Avatar
+                size="9"
+                src="https://images.unsplash.com/photo-1481214110143-ed630356e1bb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                radius="full"
+                fallback={username[0].toUpperCase()}
+                style={{ width: "20rem", height: "20rem" }}
+              />
             </Box>
+
+            {/* <Separator orientation="vertical" style={{height: "90%"}} /> */}
+
+            <Flex
+              width="40%"
+              overflow="scroll"
+              height="100%"
+              justify="start"
+              mx="3"
+              align="center"
+            >
+              <Box>
+                <Heading
+                  weight="bold"
+                  as="h1"
+                  style={{ fontSize: "3rem", height: "40%" }}
+                  my="4"
+                >
+                  {username}
+                </Heading>
+                <Heading
+                  weight="regular"
+                  as="h2"
+                  size="4"
+                  style={{ color: "gray" }}
+                  my="4"
+                >
+                  {currentUser.email}
+                </Heading>
+                <Flex justify="start" gap="2" mt="6rem">
+                  <Button color="crimson" style={{ width: "8rem" }}>
+                    Edit Profile
+                  </Button>
+
+                  <Button onClick={handleLogout} style={{ width: "8rem" }}>
+                    Logout
+                  </Button>
+                </Flex>
+              </Box>
+            </Flex>
           </Flex>
-        </Flex>
-        <Separator style={{width: "90%", margin: "auto"}}/>
-      </Box>
+          <Separator style={{ width: "90%", margin: "auto" }} />
+        </Box>
       </Flex>
     </Box>
   );
