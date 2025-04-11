08a2d25ea721dcfbb0cc1d8898eb31c40f29219d
Aditya Sutar <aditya1.sutar@outlook.com>
2025-04-11 19:40:07 +0530

    Update LogoutButton.jsx

--- client/src/components/Home/Auth/LogoutButton.jsx
+++ client/src/components/Home/Auth/LogoutButton.jsx
@@ -1,36 +1,35 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { ExitIcon } from "@radix-ui/react-icons";
-function LogoutButton ({handleLogout}) {
-    return (
-          <AlertDialog.Root>
-            <AlertDialog.Trigger>
-              <Button>
-                Logout
-                <ExitIcon />
-              </Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-              <AlertDialog.Title>Logout?</AlertDialog.Title>
-              <AlertDialog.Description size="2">
-                Are you sure? This will log you out of CodeStream!
-              </AlertDialog.Description>
+function LogoutButton({ handleLogout }) {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button>
+          Logout
+          <ExitIcon />
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Logout?</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This will log you out of CodeStream!
+        </AlertDialog.Description>
 
-              <Flex gap="3" mt="4" justify="end">
-                <AlertDialog.Cancel>
-                  <Button variant="soft" color="gray">
-                    Cancel
-                  </Button>
-                </AlertDialog.Cancel>
-                <AlertDialog.Action>
-                  <Button variant="solid" color="red" onClick={handleLogout}>
-                    Logout 
-                  </Button>
-                </AlertDialog.Action>
-              </Flex>
-            </AlertDialog.Content>
-          </AlertDialog.Root>
-    )
-
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" onClick={handleLogout}>
+              Logout
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 }
 
 export default LogoutButton;
